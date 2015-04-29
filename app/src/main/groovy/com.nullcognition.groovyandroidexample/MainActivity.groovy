60318657bb7538aa3231c0ef73da7ae5ab3512fd
ersin <ersin_the_ertan@hotmail.com>
2015-04-29 09:56:35 -0400

    Added GPars - groovy parallel to build/project.
    Actor example to verify build and run

--- app/src/main/groovy/com.nullcognition.groovyandroidexample/MainActivity.groovy
+++ app/src/main/groovy/com.nullcognition.groovyandroidexample/MainActivity.groovy
@@ -1,40 +1,70 @@
-package com.nullcognition.groovyandroidexample;
+package com.nullcognition.groovyandroidexample
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.os.Bundle
+import android.support.v7.app.ActionBarActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import groovyx.gpars.MessagingRunnable
+import groovyx.gpars.actor.DynamicDispatchActor
 
 public class MainActivity extends ActionBarActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MyStatelessActor actor = new MyStatelessActor();
+        actor.start();
+        actor.send("Hello");
+        actor.sendAndWait(10);
+        actor.sendAndContinue(10.0, new MessagingRunnable<String>() {
+            @Override
+            protected void doRun(final String s) {
+                Log.e("TAG", "Received a reply " + s);
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if(id == R.id.action_settings){
+        if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+}
+
+class MyStatelessActor extends DynamicDispatchActor {
+    public void onMessage(final String msg) {
+        System.out.println("Received " + msg);
+        replyIfExists("Thank you");
+    }
+
+    public void onMessage(final Integer msg) {
+        System.out.println("Received a number " + msg);
+        replyIfExists("Thank you");
+    }
+
+    public void onMessage(final Object msg) {
+        System.out.println("Received an object " + msg);
+        replyIfExists("Thank you");
     }
 }
